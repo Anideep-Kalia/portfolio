3f52a249f2a7c35d9febb6c840ae7f457347cc22
Anideep <anideepkalia@gmail.com>
2023-07-24 11:47:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import TechStack from "./components/TechStack";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <TechStack />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
